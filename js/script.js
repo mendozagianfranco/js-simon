5c82941905f0281380c3f44688d93f8ed5a4a28f
mendozagianfranco <mendozagianfranco88@gmail.com>
2025-03-06 16:07:58 +0100

    feat: add numbers random and create list numbers

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const formElement = document.getElementById('answers-form');
 const messageElement = document.getElementById('message');
 const listElement = document.getElementById('numbers-list');
 let count = 10;
+const numbers = [];
 
 const countdown = setInterval(() => {
     countDownElement.innerText = count;
@@ -10,6 +11,30 @@ const countdown = setInterval(() => {
     if (count === -1) {
         clearInterval(countdown);
         formElement.classList.remove('d-none');
+        listElement.classList.add('d-none');
     }
 }, 1000);
 
+
+function getNumbers() {
+    for (let i = 0; i < 5; i++) {
+        numbers.push(getRndInteger(1, 50));
+    }
+    return numbers;
+}
+
+const listNumbers = document.createDocumentFragment();
+function createListNumbers() {
+    getNumbers().forEach(e => {
+        let liElement = document.createElement('li');
+        liElement.append(e);
+        listNumbers.appendChild(liElement);
+
+    });
+    listElement.appendChild(listNumbers);
+}
+
+createListNumbers();
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
